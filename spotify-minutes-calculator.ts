47f5199cf3f8dc844750c67ee8ee75b1cd16e2b6
Amalya Megerman <amalyamegerman@gmail.com>
2023-04-23 21:30:50 -0400

    refactor getFiles()

--- spotify-minutes-calculator.ts
+++ spotify-minutes-calculator.ts
@@ -3,51 +3,42 @@ import StreamingHistory from "./src/streaminghistory-interface";
 
 class SpotifyCalculator {
   constructor(public dirname: string) {}
-  public minutes: number = 0;
 
   async getFiles(dirname: string): Promise<StreamingHistory[]> {
     const filenames: string[] = await fs.promises.readdir(dirname);
 
-    let streamingHistory: StreamingHistory[] = [];
-    await Promise.all(
-      filenames.map(async (fileName: string) => {
-        streamingHistory.push(
-          ...JSON.parse(
-            await fs.promises.readFile(
-              `./streaming-history/${fileName}`,
-              "utf8"
-            )
-          )
-        );
-      })
+    const streamingHistoryPromises = filenames.map(
+      async (fileName: string): Promise<StreamingHistory[]> =>
+        JSON.parse(
+          await fs.promises.readFile(`./streaming-history/${fileName}`, "utf8")
+        )
     );
-    return streamingHistory;
+
+    return (await Promise.all(streamingHistoryPromises)).flat();
   }
 
-  getTotalMs(streamingHistory: StreamingHistory[]) {
+  getTotalMs(streamingHistory: StreamingHistory[]): number {
     return streamingHistory.reduce(
       (totalMs, track) => totalMs + track.msPlayed,
       0
     );
   }
 
-  convertTime(totalMsPlayed: number) {
-    if (totalMsPlayed > 0) {
-      this.minutes = Math.floor(totalMsPlayed / 60000);
-    }
+  convertTime(totalMsPlayed: number): number {
+    return Math.floor(totalMsPlayed / 60000);
   }
 
-  displayResults() {
+  displayResults(minutes: number) {
     console.log(
-      `You spent ${this.minutes} minutes streaming on Spotify in the last year!`
+      `You spent ${minutes} minutes streaming on Spotify in the last year!`
     );
   }
 
   async execute() {
     const streamingHistory = await this.getFiles(this.dirname);
     const totalMs = this.getTotalMs(streamingHistory);
-    this.convertTime(totalMs);
-    this.displayResults();
+    let totalMin = this.convertTime(totalMs);
+    this.displayResults(totalMin);
   }
 }
 
